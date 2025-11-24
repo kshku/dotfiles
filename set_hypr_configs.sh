841f5cffd2b7e80ad70ca3837ac2e92137e72665
kshku <1211shree@gmail.com>
2025-11-24 21:31:04 +0530

    trying to setup dark mode

--- set_hypr_configs.sh
+++ set_hypr_configs.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 directories="hypr waybar dunst"
-packages="hyprland hypridle hyprcursor hyprlock hyprpaper hyprpolkitagent xdg-desktop-portal-hyprland dunst waybar cliphist"
+packages="hyprland hypridle hyprcursor hyprlock hyprpaper hyprpolkitagent xdg-desktop-portal-hyprland dunst waybar cliphist adw-gtk-theme"
+aur_packages="hyprqt6engine"
 
 echo "Copying config files..."
 
@@ -9,6 +10,11 @@ for dir in $directories
 do
     if [ -d "$HOME/.config/$dir" ]
     then
+        if [ -d "$HOME/.config/$dir.bak" ]
+        then
+            echo "Removing old backup..."
+            rm -rf "$HOME/.config/$dir.bak"
+        fi
         mv -i -T "$HOME/.config/$dir" "$HOME/.config/$dir.bak"
     fi
     cp -r ".config/$dir" "$HOME/.config/$dir"
@@ -16,3 +22,4 @@ done
 
 echo "Trying to install required packages using pacman..."
 sudo pacman -S --needed $packages
+yay -S --needed $aur_packages
